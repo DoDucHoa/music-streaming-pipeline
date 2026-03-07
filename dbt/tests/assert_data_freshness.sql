d1f49c8ffca5b54fb93dd99383100a950731ee1c
Do Duc Hoa <hhoa0978@gmail.com>
2026-03-07 11:09:28 +0700

    Apply updated .gitignore

--- dbt/tests/assert_data_freshness.sql
+++
@@ -1,9 +0,0 @@
--- Test: Check data freshness - ensure we have recent data
--- This test warns if the most recent data is older than 2 days
-
-SELECT
-    MAX(event_date) AS latest_date,
-    CURRENT_DATE() AS today,
-    DATE_DIFF(CURRENT_DATE(), MAX(event_date), DAY) AS days_behind
-FROM {{ ref('fct_song_plays') }}
-HAVING DATE_DIFF(CURRENT_DATE(), MAX(event_date), DAY) > 2
